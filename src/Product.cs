822896c94a8cb8a17c3aff7e7fa2678b2471a251
Reginaldo Alves de Barros <reginaldo.barros@fcamara.com.br>
2017-11-06 12:38:34 -0200

    add files

---
+++ src/Product.cs
@@ -0,0 +1,9 @@
+namespace CsvReaderSample 
+{
+    public class Product
+    {
+        public int Id { get; set; }  
+        public string Name { get; set; }   
+        public string Price { get; set; }
+    }
+}
---
+++ src/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.IO;
+using CsvHelper;
+
+namespace CsvReaderSample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var currentDirectory = Directory.GetCurrentDirectory();
+
+            var filePath = Path.Combine(currentDirectory, "Files\\catalogo.csv");          
+
+            using (var textReader = File.OpenText(filePath))
+            {
+                var csv = new CsvReader(textReader);
+                csv.Configuration.Delimiter = ";";
+                // csv.Configuration.HasHeaderRecord = false;
+
+                // Prepare Header
+                csv.Configuration.PrepareHeaderForMatch = header => header?.Trim();
+                csv.Configuration.PrepareHeaderForMatch = header => header.Replace(" ", string.Empty);
+                csv.Configuration.PrepareHeaderForMatch = header => header.Replace("_", string.Empty);
+                csv.Configuration.PrepareHeaderForMatch = header => header.ToLower();
+
+                var records = csv.GetRecords<Product>();
+
+                foreach (var product in records)
+                {
+                    var isDecimal = decimal.TryParse(product.Price, out decimal result);
+
+                    Console.WriteLine($"{product.Id} - {product.Name} - {product.Price} - {isDecimal}");
+                }
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
